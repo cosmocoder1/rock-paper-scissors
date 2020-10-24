98d949e8e0f5cd5e346f8ad071919cdf8baf6262
Nathan Lucy <cosmocoder1@gmail.com>
2020-10-24 10:26:18 -0500

    final commit

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
+// define new variables that store scores and winner status for final display
+var computerPlay;
+var winnerToggle = 0;
+var userScore = 0;
+var computerScore = 0;
 
 // computer randomly generates a value in order to makes its play from predefined options
-
 const getComputerPlay = () => {
   let plays = ['rock', 'paper', 'scissors'];
   let randomNum = Math.floor(Math.random() * 3);
-  let computerPlay = plays[randomNum];
-  return computerPlay;
+  computerPlay = plays[randomNum];
 }
 
-
-// define new variables that store scores and winner status for final display
-
-var computerPlay = getComputerPlay();
-var winnerToggle = 0;
-
-
 //game on!
-
 const gameRound = (play, computerPlay) => {
 
+  
   let userPlay = play;
 
 if (userPlay === computerPlay) {
@@ -44,7 +40,6 @@ if (userPlay === computerPlay) {
   }
 }
 
-
 // applause function 
 
 const audioResponse = (sound) => {
@@ -52,7 +47,6 @@ const audioResponse = (sound) => {
   var snd = new Audio(path + sound + ".wav");
   snd.play();
 }
-
 
 // post result function
 
@@ -70,116 +64,135 @@ const postResult = (input) => {
   }
 }
 
-// hide middle graphic onclick
-
+// hide/show middle graphic onclick
 const hideMiddle = () => {
   middle.style.display = 'none';
+  score.style.display = 'none';
+  setTimeout(function(){middle.style.display = 'block';score.style.display = "block"}, 3000);
 }
 
+// hide/show score
+const scoreKeep = () => {
+  if (winnerToggle === 1) {
+    userScore += 1;
+  } else if (winnerToggle === -1) {
+    computerScore += 1;
+  } 
+  score.innerHTML = `you : ${userScore} \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 computer : ${computerScore}`;
+}
+
+// hide/show buttons
 const hideButtons = () => {
   buttons.style.display = 'none';
+  setTimeout(function(){buttons.style.display = 'flex'}, 3000);
 }
 
-// show userPlay's image onclick
-
+// show/hide userPlay's image on click
 const showUserPlay = (play) => {
   let userPlayImg = document.querySelector('.userPlay');
   userPlayImg.src = `/images/${play}U.png`;
+  setTimeout(function(){userPlayImg.src = ''}, 3000);
 }
 
-// show computerPlay's image onclick
-
+// show/hide computerPlay's image on click
 const showComputerPlay = () => {
   let computerPlayImg = document.querySelector('.computerPlay');
   computerPlayImg.src = `/images/${computerPlay}C.png`;
+  setTimeout(function(){computerPlayImg.src = ''}, 3000);
 }
 
-// show score result with text 
-
+// show/hide score result with text 
 const showResult = () => {
   result.style.display = 'block';
+  setTimeout(function(){result.style.display = 'none'}, 3000);
 }
 
 // define buttons
-
 let buttons = document.querySelector('.buttons');
-let middle = document.querySelector('#middle');
+let middle = document.querySelector('.middle');
+let score = document.querySelector('#score');
+let game = document.querySelector('#gameOutcome');
 let rockBtn = document.querySelector('#rock');
 let paperBtn = document.querySelector('#paper');
 let scissorsBtn = document.querySelector('#scissors');
 
 // reload page
+const gameResult = () => {
+  setTimeout(function () {
+    if (computerScore === 3 || userScore === 3) {
+      setTimeout(function () {
+        if (userScore > computerScore) {
+          game.innerHTML = "you're the winner!"
+        } else if (userScore < computerScore) {
+          game.innerHTML = 'the computer is the winner...'
+        }
+    });
+  }
+}, 3000);
+}
 
+// reload page
 const reload = () => {
-  document.location.reload();
+  if (computerScore === 3 || userScore === 3) {
+    setTimeout(function () {
+      document.location.reload();
+    }, 6000);
+  }
 }
 
-const refresh = () => {
-  setTimeout(reload, 3000);
-
-}
-
-// add events to buttons
-
+// attach button events
 rockBtn.addEventListener('click', () => {  
     
+    getComputerPlay();
     gameRound('rock', computerPlay);
+    scoreKeep();
     postResult('rock');
     hideMiddle();
     hideButtons();
     showUserPlay('rock');
     showComputerPlay();
     showResult();
-    refresh();
+    gameResult();
+    reload();
   }
 );
 
 paperBtn.addEventListener('click', () => {
     
+    getComputerPlay();
     gameRound('paper', computerPlay);
+    scoreKeep();
     postResult('paper');
     hideMiddle();
     hideButtons();
     showUserPlay('paper');
     showComputerPlay();
     showResult();
-    refresh();
+    gameResult();
+    reload();
   }
 );
 
 scissorsBtn.addEventListener('click', () => {
     
+    getComputerPlay();
     gameRound('scissors', computerPlay);
+    scoreKeep();
     postResult('scissors');
     hideMiddle();
     hideButtons();
     showUserPlay('scissors');
     showComputerPlay();
     showResult();
-    refresh();
+    gameResult();
+    reload();
   }
 );
 
 
 
-/*
 
-// 5 game sequence function
 
-const gameOn = () => {
-  for (let i = 0; i < 6; i++) {
-    gameRound();
-    if (winnerToggle === 1) {
-  
-      console.log('You won this round!');
-    } else if (winnerToggle === -1) { 
-      console.log('You lost this round!')
-    } else {console.log('you tied this round!')}
-    console.log(`User: ${userScore} Computer: ${computerScore}`);
-  }
-}
-
-*/
 
 
 
